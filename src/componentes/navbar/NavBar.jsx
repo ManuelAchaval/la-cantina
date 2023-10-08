f3302e6ecad8731e6682a0598569de7f5adcfc66
ManuelAchaval <manuachaval@hotmail.com>
2023-10-08 13:08:50 -0300

    agrego BEM y modificaciones de estilo

--- src/componentes/navbar/NavBar.jsx
+++ src/componentes/navbar/NavBar.jsx
@@ -3,31 +3,32 @@ import CartWidget from '../cartwidget/CartWidget'
 
 const NavBar = (params) => {
 
-    if (params.type == "footer") {
+    if (params.type === "footer") {
         return (
             <>
                 <div className='footer'>
-                    <ul>
-                        <li>Contacto</li>
-                        <li><p>Manuel Achaval </p>
-                            <p>©Copyright 2023</p></li>
-                        <li>Sobre nosotros</li>
-                        
-                    </ul>
+                    <ul className='footer__ul'>
+                        <li className='footer__li'>Contacto</li>
+                        <li className='footer__li'><p>Manuel Achaval</p>
+                        </li>
 
+                        <li className='footer__li'>Sobre nosotros</li>
+
+                    </ul>
+                    <p className='footer__p'>©Copyright 2023</p>
                 </div>
             </>
         )
     }
     else {
         return (
-            <nav>
-                <ul>
-                    <li><h1>La Cantina</h1></li>
-                    <li><a href="#">Accesorios</a></li>
-                    <li><a href="#">Ropa</a></li>
-                    <li><a href="#">Coleccionables</a></li>
-                    <li><a href="#">Replicas</a></li>
+            <nav className='nav'>
+                <ul className='nav__ul'>
+                    <li className='nav__li'><h1>La Cantina</h1></li>
+                    <li className='nav__li'><a href="#">Accesorios</a></li>
+                    <li className='nav__li'><a href="#">Ropa</a></li>
+                    <li className='nav__li'><a href="#">Coleccionables</a></li>
+                    <li className='nav__li'><a href="#">Replicas</a></li>
                     <CartWidget />
                 </ul>
 
